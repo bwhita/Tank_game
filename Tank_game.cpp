4dc46919dbf4646d589ee2317ded5faf6a6df1de
Brandon James Whitaker <whitak44@purdue.edu>
2025-05-06 13:25:42 -0400

    Tank game

--- Tank_game.cpp
+++ Tank_game.cpp
@@ -2,19 +2,65 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+void LoadTex(Texture& tex, string filename) {
+    if (!tex.loadFromFile(filename)) {
+        cout << "Could not load " << filename << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RenderWindow window(VideoMode(1000, 1000), "Tank Assualt");
+    Texture worldTex;
+    LoadTex(worldTex, "images/tank_mat.png");
+    Sprite worldSprite;
+    worldSprite.setTexture(worldTex);
+    World world(Vector2f(0, 0));
+    int score(0);
+    PhysicsSprite& usertank = *new PhysicsSprite();
+    Texture ftankTex;
+    LoadTex(ftankTex, "images/user_tank.png");
+    usertank.setTexture(ftankTex);
+    Vector2f sz = usertank.getSize();
+    usertank.setCenter(Vector2f(500, 500));
+
+    PhysicsSprite shell;
+    Texture shellTex;
+    LoadTex(shellTex, "images/tank_shell.png");
+    shell.setTexture(shellTex);
+    bool drawingshell(false);
+
+    PhysicsRectangle top;
+    top.setSize(Vector2f(1000, 10));
+    top.setCenter(Vector2f(500, 5));
+    top.setStatic(true);
+    world.AddPhysicsBody(top);
+
+    PhysicsRectangle bottom;
+    bottom.setSize(Vector2f(5, 1000));
+    bottom.setCenter(Vector2f(5, 500));
+    top.setStatic(true);
+    world.AddPhysicsBody(bottom);
+
+    PhysicsRectangle left;
+    left.setSize(Vector2f(10, 1000));
+    left.setCenter(Vector2f(5, 500));
+    left.setStatic(true);
+    world.AddPhysicsBody(left);
+
+    PhysicsRectangle right;
+    right.setSize(Vector2f(10, 1000));
+    right.setCenter(Vector2f(1000, 500));
+    right.setStatic(true);
+    world.AddPhysicsBody(right);
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
